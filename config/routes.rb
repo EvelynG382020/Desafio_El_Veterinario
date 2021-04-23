3749d36ab206a519b7fb94652b190991796f1c33
evelyn gomez <evelyngomezfrez@gmail.com>
2021-04-23 19:17:18 -0400

    corrigiendo el codigo

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'pages/index'
-  root 'pages#index'
 
   resources :pet_histories
   resources :pets 
   resources :clients
+
+  get 'pages/index'
+  root 'pages#index'
 
 end
